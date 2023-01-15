ef91694a86150852be53cfc3f362ca3991f8cd5f
unknown <shannoncoleman16@gmail.com>
2023-01-15 18:52:36 -0500

    added app use function

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Note Taker');
 });
