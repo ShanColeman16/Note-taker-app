a6e887e3f3703ddb364caa10a24d67624fa50d5b
unknown <shannoncoleman16@gmail.com>
2023-02-17 15:21:16 -0500

    set get to read and return saves notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,18 @@ var uniqid = require('uniqid');
 
 module.exports = ( app )  => {
 
+  app.get('/api/notes', (req, res)  => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+  });
+
+  app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db =JSON.parse.apply(db);
+    res.json(db);
   
+  }
+  
+  )
 
 
 
