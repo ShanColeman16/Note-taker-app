417f8d12a2cfca1eff2341d1a001cf93dec99237
unknown <shannoncoleman16@gmail.com>
2023-02-17 13:56:04 -0500

    added routes folder

--- server.js
+++ server.js
@@ -12,32 +12,34 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: false }));
 app.use(express.json());
 
-app.get('/api/notes', (req,res) => {
-  const data = fs.readFileSync('./db/db.json', 'utf-8');
-  const notes = JSON.parse(data);
-  res.json(notes);
- });
-
- app.post('/api/notes', (req,res) => {
-  const data = fs.readFileSync('./db/db.json', 'utf-8');
-  const notes = JSON.parse(data);
-  const newNotes = {
-    ...req.body,
-    id: uuid4()
-  };
-
-  notes.push(newNotes);
-  const stringifyedNotes = JSON.stringify(notes, null, 2);
-  fs.writeFileSync('./db/db.json', 'utf8');
-  res.json('Saved');
- });
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+app.post('/api/notes', (req,res) => {
+  const data = fs.readFileSync('./db/db.json', 'utf-8');
+  const notes = JSON.parse(data);
+  const newNotes = {
+    ...req.body,
+};
+
+
+app.get('/api/notes', (req, res) => {
+  const data = fs.readFileSync('./db/db.json', 'utf-8');
+  const notes = JSON.parse(data);
+  res.json(notes);
+ })
+
+  notes.push(newNotes);
+  const stringifyedNotes = JSON.stringify(notes, null, 2);
+  fs.writeFileSync('./db/db.json', 'utf8');
+  res.json('Saved');
 });
+
+
 
 app.listen(PORT, () => console.log('runing at port 3001'));
